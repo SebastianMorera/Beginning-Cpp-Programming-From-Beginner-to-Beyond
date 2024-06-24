1848f70553777066c643e5ac0107da04b63f56f0
Sebastian Morera <sebastianmorera@Sebastians-MBP.hitronhub.home>
2024-06-24 10:15:48 -0400

    Getting started section done

---
+++ Getting_Started/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int favorite_number = 0;
+    cout << "Enter your favorite number between 1 and 100: ";
+    cin >> favorite_number;
+    cout << "Amazing!! That's my favorite number too! " << endl;
+    cout << "No really!!, " << favorite_number << " is my favorite number!" << endl;
+
+    return 0;
+}
